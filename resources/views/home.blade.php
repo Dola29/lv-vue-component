08f03a6bc5cda0b39621e5e6962a093c8f90b59e
Dola29 <danielosvaldoleal@hotmail.com>
2018-04-26 22:58:30 -0400

    Home design with Bootstrap 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,46 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel - Components  VueJS</title>
 
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('css/app.css') }}">
         
     </head>
     <body>
-        
-        <script src="{{asset('js/app.js')}}"></script>
+        <div class="container">
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div id="app">
+                        <h2 class="text-center">Captura tus ideas</h2> 
+                         <div class="card card-body bg-light">
+                         <h4>En que estas pensando?</h4>
+                            <form >
+                                <div class="input-group">
+                                    <input type="text" class="form-control input-sm" maxlength="256">
+                                    <span class="input-group-append">
+                                        <button type="submit" class="btn btn-primary btn-sm">Agregar</button>
+                                    </span>
+                                </div>
+                            </form>    
+                            <hr>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <p>
+                                        <small class="text-muted">
+                                            <em>Hace un minuto</em>                                           
+                                        </small>
+                                        Mi nueva idea
+                                    </p>
+                                </li>
+                            </ul>                         
+                         </div>   
+                    </div>
+                </div>    
+            </div>
+        </div>
+    
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
